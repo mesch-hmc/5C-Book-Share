10e0e3c7f543e5c0774e4a0ea560d84bb67711a8
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-03-19 08:49:12 -0700

    OmniAuth: Implement Facebook Log In
    * Preparation to easily obtain user facebook link
    * Improve user experience

--- TextbookSharing/app/models/user.rb
+++ TextbookSharing/app/models/user.rb
@@ -2,15 +2,25 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :textbooks
 
   # # Data validation
-  validates :name, presence: true
-  validates :college, presence: true
-  validates :fblink, presence: true
-  validates :fblink, format: { with: /https:\/\/www.facebook.com(.*)/, message: "invalid facebook link" }
+  #validates :name, presence: true
+  #validates :college, presence: true
+  #validates :fblink, presence: true
+  #validates :fblink, format: { with: /https:\/\/www.facebook.com(.*)/, message: "invalid facebook link" }
 
   COLLEGES = ['CGU', 'CMC', 'HMC', 'KGI', 'POM', 'PTZ', 'SCR']
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.fblink = auth.info.urls["Facebook"]
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+    end
+  end
 end
--- TextbookSharing/db/schema.rb
+++ TextbookSharing/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214213248) do
+ActiveRecord::Schema.define(version: 20160319035202) do
 
   create_table "textbooks", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160214213248) do
     t.string   "fblink",                 default: "", null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
